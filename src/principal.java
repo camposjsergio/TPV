f183929f32f06a1dcbb74cfa73eee8570be93a02
Mañana <Maana@Aula1-12>
2012-01-18 13:04:57 +0100

    Empezando proyecto

---
+++ src/principal.java
@@ -0,0 +1,12 @@
+
+public class principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Había una vez...");
+	}
+
+}
